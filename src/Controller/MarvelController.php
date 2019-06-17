2edfae584ba7cb6e2cfe0b449d511d857bef6f94
jspourre <jean-sebastien.pourre@univ-lyon1.fr>
2019-06-18 01:42:34 +0200

    update for using a video game api

--- src/Controller/MarvelController.php
+++ src/Controller/MarvelController.php
@@ -6,85 +6,105 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
 
-
+/**
+ * This Controller is a client for https://api-docs.igdb.com/ 
+ */
 class MarvelController extends AbstractController
 {   
-    private  $api ="00281aa6705babdd74b9e29b3013c965";
-    private  $secret = "8e67059aef511b5a967e57293dcaf267df3b0d91";
-    private $ts ;
-
-    function __construct() 
-    {
-        $this->ts = strval(time());
-    }
+    /*
+    * Param Api Key
+    */
+    private  $api ="bb80a4bd76d416af0b7bff625dd867b7";
 
     /*
-    * method to use curl to make a request and return a json
+    * Param Request Url
     */
-    public function getJson($link) 
+    private $link = "https://api-v3.igdb.com/games";
+
+    /*
+    * method to make a find a list
+    * @offset = int of offset
+    */
+    public function findALL(int $offset=0 )
     {
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', $link,
-        [ "query" => [
-            "apikey" => $this->api,
-            "ts" => $this->ts,
-            "hash" => $this->makeHash(),
-        ],
+        $response = $httpClient->request('POST', $this->link,
+        [ 
         'headers' => [
-            'Content-Type' => 'text/plain',
-            'Accept'=> '*/*',
-            'Accept-Encoding' => 'gzip',
-
+            'Content-Type' => 'application/json',
+            'user-key' => $this->api,
         ],
-        ]);  
-        $content = $response->getContent(false);
-       
-        return $response;
+        'body' => "fields id, genres.*,summary, artworks.*, storyline, name, platforms.*, screenshots.*; 
+                    limit 20;
+                    offset".$offset .";
+                    where genres != n;
+                    where artworks != n;
+                    where platforms != n;
+                    where screenshots != n;",
+
+        ]);
+
+        return $this->getJson($response);
     }
 
     /*
-    * method to make an md5 hash
+    * method to make a find one element
+    * @id = int of id
     */
-    private function makeHash() : ?string
+    public function findById(int $id )
     {
-        $ts = time();
-        $hash = md5($this->ts.$this->secret.$this->api);
-        return $hash;
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('POST', $this->link.'/',
+        [ 
+        'headers' => [
+            'Content-Type' => 'application/json',
+            'user-key' => $this->api,
+        ],
+        'body' => "fields id, genres.*,summary, artworks.*, storyline, name, platforms.*, screenshots.*; 
+                    where id = ".$id.";",
+        ]);
+        return $this->getJson($response);
+    }
+
+    /*
+    * method to use decode json
+    */
+    public function getJson($response)
+    {
+                
+        $content = json_decode($response->getContent());
+        $response->cancel();
+        return $content;
     }
 
     /**
      * All character for Marvel API
      * 
-     * @Route("/index/{offset}", name="index", requirements={"offset"="\d+"})
+     * @Route("/index/{page}", name="index", requirements={"offset"="\d+"})
      */
-    public function index($offset = 100)
+    public function index($page = 5)
     {
-
-        $link = "http://gateway.marvel.com/v1/public/characters?limit=20&offset=".$offset;
-        
-        $query = $this->getJson($link);
-
+        $offset = $page *20;
+                
+        $query = $this->findALL($offset);
+     
 
         return $this->render('marvel/index.html.twig', [
-            'controller_name' => 'IndexController',
             'objects' => $query,
+            'nb_page' => $page,
         ]);
     }
 
     /**
      * Detail View for one character
      * 
-     * @Route("/characters/{id}", name="detailView",requirements={"id"="\d+"})
+     * @Route("/games/{id}", name="detailView",requirements={"id"="\d+"})
      */
-    public function detailView($id)
+    public function detailView(int $id)
     {
-
-        $link = "http://gateway.marvel.com/v1/public/characters/".$id."?";
-        
-        $query = $this->getJson($link);
+        $query = $this->findById($id);
 
         return $this->render('marvel/detail.html.twig', [
-            'id' => $id,
             'object' => $query,
         ]);
     }
