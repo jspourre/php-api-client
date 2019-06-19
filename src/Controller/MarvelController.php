0634957be21a3ac7182b39a567e664bc6da2929b
jspourre <jean-sebastien.pourre@univ-lyon1.fr>
2019-06-19 13:26:12 +0200

    Finalising app

--- src/Controller/MarvelController.php
+++ src/Controller/MarvelController.php
@@ -14,37 +14,52 @@ class MarvelController extends AbstractController
     /*
     * Param Api Key
     */
-    private  $api ="bb80a4bd76d416af0b7bff625dd867b7";
+    static private  $api ="bb80a4bd76d416af0b7bff625dd867b7";
 
     /*
     * Param Request Url
     */
-    private $link = "https://api-v3.igdb.com/games";
+    static public $link = "https://api-v3.igdb.com/games";
 
     /*
-    * method to make a find a list
+    * Param header for request
+    */
+    public $header;
+
+    public function __construct()
+    {
+        $this->header = array(
+            'Content-Type' => 'application/json',
+            'user-key' => self::$api,
+        );
+    }
+
+    /*
+    * method to make a find all list
     * @offset = int of offset
     */
     public function findALL(int $offset=0 )
-    {
+    {   
+        if($offset > 150) //limation for free use of api
+        {
+            $offset = 150;
+        }
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('POST', $this->link,
+        $response = $httpClient->request('POST', self::$link,
         [ 
-        'headers' => [
-            'Content-Type' => 'application/json',
-            'user-key' => $this->api,
-        ],
-        'body' => "fields id, genres.*,summary, artworks.*, storyline, name, platforms.*, screenshots.*; 
-                    limit 20;
-                    offset".$offset .";
-                    where genres != n;
-                    where artworks != n;
-                    where platforms != n;
-                    where screenshots != n;",
-
+        'headers'   =>  $this->header ,
+        'body'      =>  "fields id, genres.*,popularity ,summary, cover.*, artworks.*,
+                        storyline, name, platforms.*, screenshots.*; 
+                        limit 20;
+                        offset".$offset .";
+                        where genres != n;
+                        where artworks != n;
+                        where platforms != n;
+                        where screenshots != n;
+                        sort popularity desc;",
         ]);
 
-        return $this->getJson($response);
+        return $response;
     }
 
     /*
@@ -54,41 +69,54 @@ class MarvelController extends AbstractController
     public function findById(int $id )
     {
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('POST', $this->link.'/',
+        $response = $httpClient->request('POST', self::$link.'/',
         [ 
-        'headers' => [
-            'Content-Type' => 'application/json',
-            'user-key' => $this->api,
-        ],
-        'body' => "fields id, genres.*,summary, artworks.*, storyline, name, platforms.*, screenshots.*; 
-                    where id = ".$id.";",
+        'headers'   =>  $this->header,
+        'body'      =>  "fields id, genres.*, url, summary,first_release_date, artworks.*, 
+                        storyline, name, platforms.*, screenshots.*; 
+                        where id = ".$id.";",
         ]);
-        return $this->getJson($response);
+        return $response;
+    }
+
+    /*
+    * Verification of status code and throw Exception if not 200 or 302
+    */
+    public function verifingStatus($response)
+    {
+        $statusCode = $response->getStatusCode();
+        if(!in_array($statusCode , [200,302]))
+        {
+            throw new \Exception('Error with api. Status code is :'.$statusCode);
+        }else{
+            return true;
+        }
+        
     }
 
     /*
     * method to use decode json
     */
-    public function getJson($response)
+    public function getJsonDecode($response)
     {
-                
-        $content = json_decode($response->getContent());
-        $response->cancel();
-        return $content;
+        if($this->verifingStatus($response))
+        {
+            $content = json_decode($response->getContent());
+            $response->cancel();
+            return $content;
+        }
     }
 
     /**
      * All character for Marvel API
      * 
-     * @Route("/index/{page}", name="index", requirements={"offset"="\d+"})
+     * @Route("{page}", name="index", requirements={"offset"="\d+"})
      */
     public function index($page = 5)
     {
-        $offset = $page *20;
-                
-        $query = $this->findALL($offset);
+        $offset = ($page-1) *20;        
+        $query = $this->getJsonDecode($this->findALL($offset));
      
-
         return $this->render('marvel/index.html.twig', [
             'objects' => $query,
             'nb_page' => $page,
@@ -102,8 +130,7 @@ class MarvelController extends AbstractController
      */
     public function detailView(int $id)
     {
-        $query = $this->findById($id);
-
+        $query = $this->getJsonDecode($this->findById($id));
         return $this->render('marvel/detail.html.twig', [
             'object' => $query,
         ]);
